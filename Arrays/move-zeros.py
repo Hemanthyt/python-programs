953de36faf7b27367b80cfe67cf39f23fcd05c49
Hemanthyt <hemanth25052004@gmail.com>
2025-01-07 21:50:21 +0530

    .

---
+++ Arrays/move-zeros.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            if nums[i] ==0:
+                nums.remove(nums[i])
+                nums.append(0)
+        return nums
+s = Solution()
+print(s.moveZeroes( nums = [0,1,0,3,12]))
