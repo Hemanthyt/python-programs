d1cc90a0be4e12ee847175c53f55ca141e5188c2
Hemanthyt <hemanth25052004@gmail.com>
2025-01-14 21:52:09 +0530

    .

--- Arrays/Longest-Consecutive-Sequence.py
+++ Arrays/Longest-Consecutive-Sequence.py
@@ -1,14 +1,14 @@
 class Solution:
     def longestConsecutive(self, nums: list[int]) -> int:
-        numSet = set(nums)
-        longest = 0
-        for n in nums:
-            if n-1 not in numSet:
+        numSet=set(nums)
+        long=0
+        for num in nums:
+            if num-1 not in numSet:
                 length=1
-                while n+length in numSet:
+                while num+length in numSet:
                     length+=1
-                longest = max(length,longest)
-        return longest
+                long=max(long,length)
+        return long
 s = Solution()
 print(s.longestConsecutive(nums = [2,20,4,10,3,4,5]))
 print(s.longestConsecutive(nums = [0,3,2,5,4,6,1,1]))
