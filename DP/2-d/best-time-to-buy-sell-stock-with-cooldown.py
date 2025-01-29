9cebb1fdb1daffadf99037ee3b81a6311dc5c1ff
Hemanthyt <hemanth25052004@gmail.com>
2025-01-29 22:44:32 +0530

    .

---
+++ DP/2-d/best-time-to-buy-sell-stock-with-cooldown.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp={}
+        def dfs(i,buying):
+            if i>=len(prices):
+                return 0
+            if (i,buying) in dp:
+                return dp[(i,buying)]
+
+            cooldown=dfs(i+1,buying)            
+            if buying:
+                buy=dfs(i+1,not buying)-prices[i]
+                return max(buy,cooldown)
+            else:
+                sell = dfs(i+2,not buying)+prices[i]
+                return max(sell,cooldown)
+        return dfs(0,True)
+
+
