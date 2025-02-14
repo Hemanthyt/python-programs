3f8a696feca4111a224c0fb1d96f8d07815b0f77
Hemanthyt <hemanth25052004@gmail.com>
2025-02-14 22:20:54 +0530

    .

---
+++ Stack/car-fleet.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+        print(pair)
+        stack = []
+        for p, s in pair:  # Reverse Sorted Order
+            stack.append((target - p) / s)
+            print(stack)
+            
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+                print(stack)
+        return len(stack)
+s = Solution()
+print(s.carFleet(target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]))
