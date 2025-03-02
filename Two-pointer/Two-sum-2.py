ef86d874deec2d2252a1832fa4e0b00e61748cbe
Hemanthyt <hemanth25052004@gmail.com>
2025-03-02 22:35:17 +0530

    .

---
+++ Two-pointer/Two-sum-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        d={}
+        for i in range(len(numbers)):
+            if numbers[i] in d:
+                return [d[numbers[i]]+1,i+1]
+            else:
+                d[target - numbers[i]]=i
+        return False
+            
+s = Solution()
+# print(s.twoSum( numbers = [1,2,3,4], target = 3))
+print(s.twoSum( numbers=[2,3,4],target=6))
