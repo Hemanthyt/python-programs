2ab73e3b981f3253068a5888842b3d1cc0184f9b
Hemanthyt <hemanth25052004@gmail.com>
2025-03-04 23:06:59 +0530

    .

--- Arrays/two-sum.py
+++ Arrays/two-sum.py
@@ -7,9 +7,7 @@ class Solution:
             else:
                 d[target-nums[i]]=i
         return False
-        
-            
-        
+                
 s = Solution()
 print(s.twoSum( nums=[-1,-2,-3,-4,-5], target = -8))
 print(s.twoSum( nums = [4,5,6], target = 10))
