48b3c09ac56b11341650855ed1f13f8808a84846
Hemanthyt <hemanth25052004@gmail.com>
2025-03-09 22:11:13 +0530

    .

--- Leetcode/Linked-List/add-two-numbers.py
+++ Leetcode/Linked-List/add-two-numbers.py
@@ -19,6 +19,7 @@ class Solution:
             val=val%10
             curr.next=ListNode(val)
 
+
             curr=curr.next
             l1=l1.next if l1 else 0
             l2=l2.next if l2 else 0
