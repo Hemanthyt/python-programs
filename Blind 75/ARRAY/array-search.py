de01fed3c3daf8d2932a8483c27a8ea89677d807
Hemanthyt <hemanth25052004@gmail.com>
2025-04-06 21:47:59 +0530

    .

---
+++ Blind 75/ARRAY/array-search.py
@@ -0,0 +1,11 @@
+
+class Solution:
+    #Complete the below function
+    def search(self,arr, x):
+        #Your code here
+        for i in range(len(arr)):
+            if arr[i] == x:
+                return i
+        return -1
+s = Solution()
+print(s.search(arr=[3, 2, 1, 56, 10000, 167],x=56))
