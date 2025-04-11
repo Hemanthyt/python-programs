ecada32dd029afc5d94e5a7ed67090a01e159822
Hemanthyt <hemanth25052004@gmail.com>
2025-04-11 22:31:44 +0530

    .

---
+++ Blind 75/ARRAY/count-the-triplets.py
@@ -0,0 +1,9 @@
+# class Solution:
+# 	def countTriplet(self, arr):
+# 		# code here
+#             n = len(arr)
+#             arr.sort()
+#             count = 0
+#             for i in range()
+# s = Solution()
+# print(s.countTriplet(arr = [0, 1, 2, 3, 1] ))
