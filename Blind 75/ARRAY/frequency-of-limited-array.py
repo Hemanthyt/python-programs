96f7846cd6ee538703a99ff0c980f7739d72b1d2
Hemanthyt <hemanth25052004@gmail.com>
2025-04-12 09:56:49 +0530

    .

---
+++ Blind 75/ARRAY/frequency-of-limited-array.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+class Solution:
+    # Function to count the frequency of all elements from 1 to N in the array.
+    def frequencyCount(self, arr):
+        #  code here
+        res=[]
+        x=Counter(arr)
+        for i in range(1,len(arr)+1):
+            if i not in x.keys():
+                res.append(0)
+            else:
+                res.append(x[i])
+        return res
+                
+        
+s = Solution()
+print(s.frequencyCount(arr = [2, 3, 2, 3, 5]))
