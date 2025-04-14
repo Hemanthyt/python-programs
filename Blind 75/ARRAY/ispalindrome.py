288e8b0a4fecd9965366a7e5289ad0f569ca5709
Hemanthyt <hemanth25052004@gmail.com>
2025-04-14 18:57:32 +0530

    .

---
+++ Blind 75/ARRAY/ispalindrome.py
@@ -0,0 +1,18 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=re.sub('[^a-zA-Z0-9]',"",s).lower()
+        l=0
+        r=len(s)-1
+        while l<r:
+            if s[l]!=s[r]:
+                return False
+            else:
+                l+=1
+                r-=1
+        return True
+        
+        
+s = Solution()
+print(s.isPalindrome(s = "A man, a plan, a canal: Panama" ))
