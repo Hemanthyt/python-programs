607ca062f3d8396d8a9dcbec05337950d0679427
Hemanthyt <hemanth25052004@gmail.com>
2025-04-19 10:36:38 +0530

    .

---
+++ Blind 75/ARRAY/majority-element.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        count = Counter(nums)
+        for num,val in count.items():
+            if val > len(nums)/2:
+                return val
+        
+            
