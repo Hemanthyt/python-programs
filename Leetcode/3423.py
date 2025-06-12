e5d4f80d8ade299daabaaf648eaa5c31b293a2c2
Hemanthyt <hemanth25052004@gmail.com>
2025-06-12 10:25:07 +0530

    .

---
+++ Leetcode/3423.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxAdjacentDistance(self, nums: list[int]) -> int:
+        nums.append(nums[0])  # Append the first element to the end
+        return max(abs(nums[i]-nums[i+1]) for i in range(len(nums)-1))
+    
+    
+s = Solution()
+print(s.maxAdjacentDistance(nums=[1,2,4]))
+print(s.maxAdjacentDistance(nums=[-2,1,-5]))
+print(s.maxAdjacentDistance(nums=[-5,-10,-5]))
