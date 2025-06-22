fea78447129e412d72a29e956573ebef72625da4
Hemanthyt <hemanth25052004@gmail.com>
2025-06-22 22:10:33 +0530

    .

--- Blind 75/ARRAY/array-search.py
+++ Blind 75/ARRAY/array-search.py
@@ -2,7 +2,6 @@
 class Solution:
     #Complete the below function
     def search(self,arr, x):
-        #Your code here
         for i in range(len(arr)):
             if arr[i] == x:
                 return i
